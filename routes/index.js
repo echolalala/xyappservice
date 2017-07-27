4aa35ab4b06363e552174f2def89e93ef96ad112
chuanbin <chuanbin@0725@qq.com>
2017-07-27 23:29:50 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',body:'我是首页内容' });
 });
 
 module.exports = router;
